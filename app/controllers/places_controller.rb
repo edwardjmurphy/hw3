6f1f96baf9384b69b531bb7d690a5746505dbe21
edwardjmurphy <edward.murphy@kellogg.northwestern.edu>
2023-02-21 17:10:10 +0000

    controller updates

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,23 @@
 class PlacesController < ApplicationController
 
+def index
+@places = Place.all
+end
 
+def new
+    @place = Place.new
+end
+
+def create
+    @place = Place.new
+    @place["city"] = params["place"]["city"]
+    @place.save
+    redirect_to "/places"
+end
+
+def show
+    @place = Place.find_by({ "id" => params["id"] })
+    @posts = Post.where({ "place_id" => @place["id"] })
+end
 
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,19 @@
 class PostsController < ApplicationController
 end
+
+
+def new
+    @post = Post.new
+    @place = Place.find_by({ "id" => params["place_id"] })
+    @post["place_id"] = @place["id"]
+end
+
+def create
+    @post = Post.new
+    @post["title"] = params["post"]["title"]
+    @post["description"] = params["post"]["description"]
+    @post["date"] = params["post"]["date"]
+    @contact["place_id"] = params["post"]["place_id"]
+    @post.save
+    redirect_to "/places/#{@post["place_id"]}"
+end
