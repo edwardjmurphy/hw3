c4142ac450e7c28a7b3f1a881f6c3f2d5ab0b4ab
edwardjmurphy <edward.murphy@kellogg.northwestern.edu>
2023-02-21 18:53:14 +0000

    edits

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,4 @@
 class PostsController < ApplicationController
-end
 
 
 def new
@@ -10,12 +9,11 @@ end
 
 def create
     @post = Post.new
-   
-
     @post["title"] = params["post"]["title"]
     @post["description"] = params["post"]["description"]
     @post["posted_on"] = params["post"]["posted_on"]
-    @contact["place_id"] = params["post"]["place_id"]
+    @post["place_id"] = params["post"]["place_id"]
     @post.save
-    redirect_to "/places"
+    redirect_to "/places/#{@post["place_id"]}"
+end
 end
