3e32205dc362b5b32f23dd060f7869e10f238896
edwardjmurphy <edward.murphy@kellogg.northwestern.edu>
2023-02-22 03:38:09 +0000

    posts formatting fixes

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
 
   resources "places"
   resources "posts"
-  get ("/", { :controller => "places", :action => "index" })
+  get("/", { :controller => "places", :action => "index" })
 end
